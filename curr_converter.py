b6903958b8184cfe14617cd5a11604920d71e32c
Adarshkumar61 <adarshb2k61@gmail.com>
2025-03-03 16:45:05 +0530

    created a curr convorter

---
+++ curr_converter.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "https://api.exchangerate-api.com/v4/latest/USD"
+req = requests.get(url)
+rates = req.json()["rates"]
+amount = float(input("Enter the amount in USD: "))
+currency = input("enter the cdoe of country (USD, INR..): ").upper()
+if currency in rates:
+    print(f"{amount}USD = {amount * rates[currency]} {currency}")
+else:
+    print("not avaliable right now soon....")
