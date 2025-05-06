3c3df700fae76c9acedd72a43129b43ccea7263f
Adarshkumar61 <adarshb2k61@gmail.com>
2025-05-06 18:42:12 +0530

    updated

--- curr_converter.py
+++ curr_converter.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://api.exchangerate-api.com/v4/latest/USD"
 req = requests.get(url)
-rates = req.json()["rates"]
+rates = req.json()["rates"] 
 amount = float(input("Enter the amount in USD: "))
 currency = input("enter the cdoe of country (USD, INR..): ").upper()
 if currency in rates:
